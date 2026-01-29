79158e995bc3ecd69d9ac0fb7030e89dbdabe7f3
okolodk <grisha.zhivot@bk.ru>
2026-01-29 10:23:43 +0300

    spread/rest in JS

--- js/main.js
+++ js/main.js
@@ -55,23 +55,99 @@ console.log("\n=== Деструктуризация в парамметрах ==
 // console.log();
 // printUser(user);
 
-const product = {
-    name : "Молоко", 
-    price : 100, 
-    category : "Молочные товары", 
-    inStock : true,
+// const product = {
+//     name : "Молоко", 
+//     price : 100, 
+//     category : "Молочные товары", 
+//     inStock : true,
+// }
+
+// const {name, price, category, inStock} = product;
+
+// console.log(name);
+// console.log(price);
+// console.log(category);
+// console.log(inStock);
+
+// function printProduct(product) {
+//     console.log(product.name);
+//     console.log(product.price);
+//     console.log(product.category);
+//     console.log(product.inStock);
+// }
+
+console.log("Spread для массивов");
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+
+// const combined = [...arr1, ...arr2];
+// console.log("Объудинённый массив:", combined);
+
+// const copy = [...arr1];
+// console.log("Copy arr:", copy);
+
+// const extended = [0, ...arr1, 7, 8];
+// console.log("Расширенный массив:", extended);
+
+console.log("Spread для объектов");
+
+// const person = {
+//     name: "Ivan",
+//     age: 30,
+// }
+// const address = {
+//     city: "Санкт-Петербург",
+//     street: "Невский проспект",
+// }
+
+// const fullInfo = {...person, ...address};
+// console.log("Полная информация:", fullInfo);
+
+// const personCopy = {...person};
+// console.log("Копия объекта:", personCopy);
+
+
+// const updated = {...person, age: 31, occupation: "Deeveloper"};
+// console.log("Обнавлённый объект:", updated);
+
+console.log("Rest оператор");
+
+// function sum(...numbers) {
+//     return numbers.reduce((total, num) => total + num, 0);
+// }
+// console.log("Sum 1, 2, 3: ", sum(1, 2, 3));
+// console.log("Sum 1, 2, 3, 4, 5: ", sum(1, 2, 3, 4, 5));
+
+// const numbers = [10, 20, 30, 40, 50];
+// const [first, second, ...rest] = numbers;
+// console.log("First number:", first);
+// console.log("Second number:", second);
+// console.log("Остольные числа:", rest);
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const combined = [...arr1, ...arr2];
+
+function  findMax(...numbers) {
+    let maxEl = -1000;
+    for (let i =0; i < numbers.length; i++) {
+        if ( maxEl < numbers[i]) {
+            maxEl = numbers[i];
+        }
+    }
+    return maxEl;
 }
 
-const {name, price, category, inStock} = product;
-
-console.log(name);
-console.log(price);
-console.log(category);
-console.log(inStock);
-
-function printProduct(product) {
-    console.log(product.name);
-    console.log(product.price);
-    console.log(product.category);
-    console.log(product.inStock);
+const person = {
+    name: "Ivan",
+    age: 30,
 }
+const address = {
+    city: "Санкт-Петербург",
+    street: "Невский проспект",
+}
+
+const fullInfo = {...person, ...address};
+console.log("Полная информация:", fullInfo);
